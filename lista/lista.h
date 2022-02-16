2ed1ecf4bb303a1d537a7f38ff823bf75a1f7456
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2022-02-16 11:50:59 -0300

    Update lista.h

--- lista/lista.h
+++ lista/lista.h
@@ -6,7 +6,7 @@
 #define  LISTA_H
 
 #include <stdlib.h>
-
+#include <stdbool.h>
 // Tipo lista armazena um endereço de um valor genérico e o próximo.
 typedef struct LISTA{
 
@@ -21,9 +21,9 @@ Lista * iniciaLista();
 // Alocar um nó da lista na memória.
 Lista * alocaLista();
 // Adiciona um novo tipo ao início da lista.
-Lista * addItemLista(Lista *lista, void * endTipo);
+bool addItemLista(Lista *lista, void * endTipo);
 // Remove um item da lista.
-Lista * removeItemLista(Lista *lista, void * endTipo);
+void removeItemLista(Lista *lista, void * endTipo);
 // Deleta todos os itens da lista liberando da memória.
 void deletaLista(Lista *lista);
 // Deleta todos os itens da lista e libera a memória, liberando os tipos alocados.
